0a29c49c1074c391753874c13dda46723b78f38e
Purva Bangad <pbangad@Purvas-MBP.attlocal.net>
2024-05-08 17:11:46 -0400

    langchain | llm

--- initial.py
+++ initial.py
@@ -1,4 +1,5 @@
-##%
-import pandas as pd
-import numpy as np
-from langchain.llms import openai
+from langchain.llms import openai #deprected
+from langchain import prompts #prompt templates with multiple options
+from langchain import chains #for sequential chains
+from langchain_community.llms import openai
+from langchain_openai import OpenAI
